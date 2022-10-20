f22a51ec841ced92b0b718fd580143509efe7082
namster75 <brianhoang@gmail.com>
2022-10-19 18:43:19 -0700

    take 4

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/there/i);
+  const linkElement = screen.getByText(/should work/i);
   expect(linkElement).toBeInTheDocument();
 });
